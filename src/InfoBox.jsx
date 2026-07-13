4cdd6e0e427abdcbee61af7c88da1936cc1a2d50
Nitin Kumar <nitink60071@gmail.com>
2026-07-13 10:24:16 +0530

    Initial commit: weather app

---
+++ src/InfoBox.jsx
@@ -0,0 +1,47 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import WbSunnyIcon from "@mui/icons-material/Wbsunny";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+
+export default function InfoBox( {info}) {
+    const INIT_URL = "https://images.unsplash.com/photo-1594156596782-656c93e4d504?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YnJva2VuJTIwY2xvdWRzJTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D"
+
+    const HOT_URL = "https://images.unsplash.com/photo-1524594081293-190a2fe0baae?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const COLD_URL = "https://images.unsplash.com/photo-1612208695882-02f2322b7fee?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    const RAIN_URL = "https://images.unsplash.com/photo-1470432581262-e7880e8fe79a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhaW55JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    
+
+    return (
+        <div className="InfoBox">
+    
+            <div className='cardContainer'>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? HOT_URL : COLD_URL}
+                    title="green iguana"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {info.city} {" "}
+                    {
+                        info.humidity > 80 ? (<ThunderstormIcon />) : info.temp > 15 ? (<WbSunnyIcon />) : (<AcUnitIcon />)
+                    }
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+                    <p>Temperature = {info.temp}&deg;C</p>
+                    <p>Humidity = {info.humidity}</p>
+                    <p>Min Temp = {info.tempMin}&deg;C</p>
+                    <p>Max Temp = {info.tempMax}&deg;C</p>
+                    <p>The weather can be described as <i>{info.weather}</i> and feels like = {info.feelsLike}&deg;C</p>
+                </Typography>
+                </CardContent>
+            </Card>
+            </div>
+        </div>
+    )
+}
---
+++ src/WeatherApp.jsx
@@ -0,0 +1,27 @@
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+import { useState } from "react";
+
+export default function WeatherApp() {
+    const [weatherInfo, setWeatherInfo] = useState({
+        city: "Delhi",
+        feelsLike: 48.95,
+        temp: 38.23,
+        tempMin: 38.23,
+        tempMax: 38.23,
+        humidity: 23,
+        weather: "broken clouds",
+    });
+
+    let updateInfo = (newInfo) => {
+        setWeatherInfo(newInfo);
+    }
+
+    return(
+        <div style={{textAlign: "center"}}>
+            <h2>Weather App by Delta</h2>
+            <SearchBox updateInfo={updateInfo}/>
+            <InfoBox info={weatherInfo}/>
+        </div>
+    )
+}
